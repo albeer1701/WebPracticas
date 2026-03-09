3e926da0be6e9c8540922125c665572ac0edd74d
albeer1701 <albertoorejon1701@gmail.com>
2026-03-09 15:06:57 +0100

    Prueba eliminación Hola Mundo

--- src/main/java/com/webpracticas/WebPracticasApplication.java
+++ src/main/java/com/webpracticas/WebPracticasApplication.java
@@ -8,10 +8,6 @@ public class WebPracticasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebPracticasApplication.class, args);
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
 	}
 
 }
