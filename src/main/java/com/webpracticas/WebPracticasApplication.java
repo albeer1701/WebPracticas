5160c85eb46dc392575ff2ec0696e5812bcbcbb1
albeer1701 <albertoorejon1701@gmail.com>
2026-03-09 13:22:08 +0100

    Primer commit Spring Boot (Web de Prácticas)

---
+++ src/main/java/com/webpracticas/WebPracticasApplication.java
@@ -0,0 +1,13 @@
+package com.webpracticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPracticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPracticasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webpracticas/WebPracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webpracticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
