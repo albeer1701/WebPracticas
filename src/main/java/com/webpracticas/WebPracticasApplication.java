4e30681f927832f1da0fc0ff6c2bcb5e1a35eaea
albeer1701 <albertoorejon1701@gmail.com>
2026-03-09 14:33:41 +0100

    Modificacion Hola Mundo

--- src/main/java/com/webpracticas/WebPracticasApplication.java
+++ src/main/java/com/webpracticas/WebPracticasApplication.java
@@ -8,6 +8,11 @@ public class WebPracticasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebPracticasApplication.class, args);
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
 	}
 
 }
